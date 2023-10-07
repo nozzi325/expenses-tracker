df89c778d64b17fc788f62ef5a98cc93d5435b78
nozzi325 <nozzia3@gmail.com>
2023-10-07 19:01:56 +0300

    T-2: Refactored ExceptionControllerAdvice: added separate method for ErrorResponse creation, added EntityExistsException handling

--- src/main/java/by/zhukovsky/expensestracker/controller/ExceptionControllerAdvice.java
+++ src/main/java/by/zhukovsky/expensestracker/controller/ExceptionControllerAdvice.java
@@ -1,6 +1,7 @@
 package by.zhukovsky.expensestracker.controller;
 
 import by.zhukovsky.expensestracker.dto.ErrorResponse;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +18,18 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> exceptionEntityNotFoundHandler(EntityNotFoundException e) {
-        String errorMessage = e.getMessage();
-        logger.error(errorMessage);
-        ErrorResponse error = new ErrorResponse(errorMessage, LocalDateTime.now());
+        ErrorResponse error = createErrorResponse(e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<ErrorResponse> exceptionEntityExistsHandler(EntityExistsException e) {
+        ErrorResponse error = createErrorResponse(e.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    private ErrorResponse createErrorResponse(String message) {
+        logger.error(message);
+        return new ErrorResponse(message, LocalDateTime.now());
     }
 }
