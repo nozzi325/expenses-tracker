ffeed8de1000f72497f6cbcd0d80880acb39dfc5
nozzi325 <nozzia3@gmail.com>
2023-10-09 20:17:05 +0300

    T-3: Created EmailSenderService

---
+++ src/main/java/by/zhukovsky/expensestracker/service/EmailSenderService.java
@@ -0,0 +1,39 @@
+package by.zhukovsky.expensestracker.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+    private final static Logger LOGGER = LoggerFactory
+            .getLogger(EmailSenderService.class);
+
+    private final JavaMailSender mailSender;
+
+    public EmailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Async
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("andrew-app@test.com");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            LOGGER.error("failed to send email", e);
+            throw new IllegalStateException("failed to send email");
+        }
+    }
+}
