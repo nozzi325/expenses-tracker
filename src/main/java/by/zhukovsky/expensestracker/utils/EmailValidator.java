48827f73184c085ff2064edab148e295830fbadd
nozzi325 <nozzia3@gmail.com>
2023-10-09 20:06:51 +0300

    T-3: Created EmailValidator

---
+++ src/main/java/by/zhukovsky/expensestracker/utils/EmailValidator.java
@@ -0,0 +1,14 @@
+package by.zhukovsky.expensestracker.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+    public static boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
