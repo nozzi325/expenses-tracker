9f606b624c9f129ecf0b0b984bcea6e81a433fea
nozzi325 <nozzia3@gmail.com>
2023-10-19 21:55:58 +0300

    T-4: Added methods to TransactionRepository for filtering transactions by date and category.

--- src/main/java/by/zhukovsky/expensestracker/repository/TransactionRepository.java
+++ src/main/java/by/zhukovsky/expensestracker/repository/TransactionRepository.java
@@ -1,9 +1,25 @@
 package by.zhukovsky.expensestracker.repository;
 
 import by.zhukovsky.expensestracker.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Page<Transaction> findByUserId(Long userId, Pageable pageable);
+
+    Page<Transaction> findByUserIdAndDateBetweenAndCategoryId(Long userId,
+                                                              LocalDate startDate,
+                                                              LocalDate endDate,
+                                                              Long categoryId,
+                                                              Pageable pageable);
+
+    Page<Transaction> findByUserIdAndDateBetween(Long userId, LocalDate startDate,
+                                                 LocalDate endDate, Pageable pageable);
+
+    Page<Transaction> findByUserIdAndCategoryId(Long userId, Long categoryId, Pageable pageable);
 }
