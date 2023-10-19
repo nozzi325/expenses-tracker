547c629a5c67fd0a834148676723155dfb68407e
nozzi325 <nozzia3@gmail.com>
2023-10-19 21:57:40 +0300

    T-4: Added a new endpoint in UserController for retrieving user transactions with optional filtering by date and category.

--- src/main/java/by/zhukovsky/expensestracker/controller/UserController.java
+++ src/main/java/by/zhukovsky/expensestracker/controller/UserController.java
@@ -1,7 +1,12 @@
 package by.zhukovsky.expensestracker.controller;
 
+import by.zhukovsky.expensestracker.entity.Transaction;
 import by.zhukovsky.expensestracker.entity.User;
+import by.zhukovsky.expensestracker.service.TransactionService;
 import by.zhukovsky.expensestracker.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +15,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
     private final UserService userService;
+    private final TransactionService transactionService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, TransactionService transactionService) {
         this.userService = userService;
+        this.transactionService = transactionService;
     }
 
     @GetMapping
@@ -46,5 +55,16 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(userService.updateUser(id, user));
+    }
+
+    @GetMapping("{id}/transactions")
+    public Page<Transaction> getUserTransactions(
+            @PathVariable(name = "id") Long userId,
+            Pageable pageable,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+            @RequestParam(required = false) Long categoryId
+    ) {
+        return transactionService.getTransactionsForUser(userId, pageable, startDate, endDate, categoryId);
     }
 }
