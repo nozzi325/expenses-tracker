f8b688581d948baa6daa4184ae398c3b46da316d
nozzi325 <nozzia3@gmail.com>
2023-10-20 21:44:09 +0300

    T-4: Added methods for references extracting in Service

--- src/main/java/by/zhukovsky/expensestracker/service/UserService.java
+++ src/main/java/by/zhukovsky/expensestracker/service/UserService.java
@@ -2,13 +2,11 @@ package by.zhukovsky.expensestracker.service;
 
 import by.zhukovsky.expensestracker.entity.User;
 import by.zhukovsky.expensestracker.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserService {
@@ -48,5 +46,13 @@ public class UserService {
 
     public boolean existsById(Long userId) {
         return userRepository.existsById(userId);
+    }
+
+    public User getReferenceById(Long userId) {
+        User user = userRepository.getReferenceById(userId);
+        if (user == null) {
+            throw new EntityNotFoundException("User with id " + userId + " not found");
+        }
+        return user;
     }
 }
