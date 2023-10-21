62edec0519d713a4406ac2690864afc83800f948
nozzi325 <nozzia3@gmail.com>
2023-10-21 15:23:38 +0300

    T-3: Created AuthUserDetailsService

---
+++ src/main/java/by/zhukovsky/expensestracker/service/auth/AuthUserDetailsService.java
@@ -0,0 +1,23 @@
+package by.zhukovsky.expensestracker.service.auth;
+
+import by.zhukovsky.expensestracker.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "Username " + username + " not found"));
+    }
+}
