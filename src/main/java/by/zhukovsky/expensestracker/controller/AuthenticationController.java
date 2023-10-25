caaf8d5c5d4dc79cce7a9f7311708a28fd168753
nozzi325 <nozzia3@gmail.com>
2023-10-25 17:01:54 +0300

    AuthenticationController: removed empty line

--- src/main/java/by/zhukovsky/expensestracker/controller/AuthenticationController.java
+++ src/main/java/by/zhukovsky/expensestracker/controller/AuthenticationController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
-
     private final AuthenticationService authenticationService;
 
     public AuthenticationController(AuthenticationService authenticationService) {
