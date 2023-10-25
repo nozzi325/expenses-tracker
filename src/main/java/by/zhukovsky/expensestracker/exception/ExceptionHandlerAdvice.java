b5ce7c2ba215c3c0cfbd02ee0c2b897fa06927df
nozzi325 <nozzia3@gmail.com>
2023-10-25 17:00:58 +0300

    ExceptionHandlerAdvice: added handler for BadCredentialsException

--- src/main/java/by/zhukovsky/expensestracker/exception/ExceptionHandlerAdvice.java
+++ src/main/java/by/zhukovsky/expensestracker/exception/ExceptionHandlerAdvice.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -32,6 +33,12 @@ public class ExceptionHandlerAdvice {
     public ResponseEntity<ErrorResponse> exceptionConfirmationTokenExpiredHandler(ConfirmationTokenExpiredException e) {
         ErrorResponse error = createErrorResponse(e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.GONE);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> exceptionBadCredentialsHandler(BadCredentialsException e) {
+        ErrorResponse error = createErrorResponse(e.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     private ErrorResponse createErrorResponse(String message) {
