f1b1a69ee63b6c2fc4223ae7132f6c793a20af59
nozzi325 <nozzia3@gmail.com>
2023-10-26 17:34:11 +0300

    T-9: Created CategoryServiceTest and added constructors to Category

--- src/main/java/by/zhukovsky/expensestracker/entity/Category.java
+++ src/main/java/by/zhukovsky/expensestracker/entity/Category.java
@@ -42,4 +42,13 @@ public class Category {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
